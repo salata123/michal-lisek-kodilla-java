1d4de9930330fc9b66eb040b7bf9a5e4a82af755
Michal <salatman13@gmail.com>
2023-05-16 00:01:06 +0200

    6.2 Added first unit test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,2 +1,41 @@
-package com.kodilla.testing;public class TestingMain {
+package com.kodilla.testing;
+
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
+public class TestingMain {
+
+    public static void main(String[] args) {
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("Test - pierwszy test jednostkowy:");
+
+        Calculator calculator = new Calculator(3,4);
+
+        Integer calculatorAddResult = calculator.add();
+
+        if (calculatorAddResult == 7){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator1 = new Calculator(10, 9);
+        Integer calculatorSubtractResult = calculator1.subtract();
+
+        if (calculatorSubtractResult == 1){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+    }
 }
--- kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -1,2 +1,37 @@
-package com.kodilla.testing.calculator;public class Calculator {
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+    int a;
+    int b;
+    int result;
+
+    public Calculator(int a, int b){
+        this.a = a;
+        this.b = b;
+    }
+
+    public int add(){
+        result = a + b;
+        return result;
+    }
+
+    public int subtract(){
+        result = a - b;
+        return result;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public int getResult() {
+        return result;
+    }
+
+
+
 }
