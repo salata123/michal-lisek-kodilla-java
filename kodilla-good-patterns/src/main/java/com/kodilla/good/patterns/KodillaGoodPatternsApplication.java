527ddf3b749caa89caabf0d0a8737fb676dd2a46
Michal <salatman13@gmail.com>
2023-07-06 16:08:02 +0200

    13.1.1 Added new module with movie titles list printing method

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/KodillaGoodPatternsApplication.java
@@ -0,0 +1,16 @@
+package com.kodilla.good.patterns;
+
+import com.kodilla.good.patterns.challenges.MovieStore;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaGoodPatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KodillaGoodPatternsApplication.class, args);
+		MovieStore movieStore = new MovieStore();
+		movieStore.movieTitlesList();
+
+	}
+}
