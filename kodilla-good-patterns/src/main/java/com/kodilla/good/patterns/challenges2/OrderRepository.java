f3c1be2f209301ddc879be13b341744d7fb3f149
Michal <salatman13@gmail.com>
2023-07-12 14:52:54 +0200

    13.4.2 fixed OrderRepository class

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges2/OrderRepository.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges2/OrderRepository.java
@@ -3,7 +3,7 @@ package com.kodilla.good.patterns.challenges2;
 import java.util.ArrayList;
 
 public class OrderRepository {
-    ArrayList<Order> orderList = new ArrayList<>();
+    private ArrayList<Order> orderList = new ArrayList<>();
     public void addOrder(Order order){
         orderList.add(order);
     }
