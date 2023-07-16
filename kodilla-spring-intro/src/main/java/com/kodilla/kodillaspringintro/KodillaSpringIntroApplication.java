7f7dc421ce60108d890a3b33fe01d50aafa328e9
Michal <salatman13@gmail.com>
2023-07-17 00:35:52 +0200

    14.1.1s Code snapshot

---
+++ kodilla-spring-intro/src/main/java/com/kodilla/kodillaspringintro/KodillaSpringIntroApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.kodillaspringintro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaSpringIntroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaSpringIntroApplication.class, args);
+    }
+
+}
---
+++ kodilla-spring-intro/src/test/java/com/kodilla/kodillaspringintro/KodillaSpringIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.kodillaspringintro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodillaSpringIntroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
