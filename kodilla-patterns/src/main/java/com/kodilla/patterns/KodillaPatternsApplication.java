ee5390a4703815d06066932469f7057184bb0941
salata123 <salatman13@gmail.com>
2023-07-24 13:21:33 +0200

    15.1.0s code snapshot

---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/KodillaPatternsApplication.java
@@ -0,0 +1,14 @@
+package com.kodilla.patterns;
+
+import com.kodilla.patterns.enums.hotel.HotelApp;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaPatternsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaPatternsApplication.class, args);
+    }
+
+}
