6d638bafc1a33fc22aaa59033bf515953700795c
Michal <salatman13@gmail.com>
2023-10-08 22:16:44 +0200

    24.2.2 Added more tests

--- kodilla-patterns2/src/test/java/com/kodilla/patterns2/decorator/pizza/PizzaOrderTestSuite.java
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/decorator/pizza/PizzaOrderTestSuite.java
@@ -8,7 +8,41 @@ import static org.junit.jupiter.api.Assertions.*;
 public class PizzaOrderTestSuite {
 
     @Test
-    void testBuildingPizza(){
+    void testBuildingPizza1(){
+        //Given
+        PizzaOrder pizzaOrder = new BasePizza();
+        pizzaOrder = new PizzaBaconDecorator(pizzaOrder);
+
+        //When
+        BigDecimal calculatedCost = pizzaOrder.getCost();
+        String pizzaIngredients = pizzaOrder.getIngredients();
+
+        //Then
+        assertEquals(new BigDecimal(20), calculatedCost);
+        assertEquals("Pizza dough with cheese and tomato sauce + bacon", pizzaIngredients);
+    }
+
+    @Test
+    void testBuildingPizza2(){
+        //Given
+        PizzaOrder pizzaOrder = new BasePizza();
+        pizzaOrder = new PizzaBaconDecorator(pizzaOrder);
+        pizzaOrder = new PizzaCheeseDecorator(pizzaOrder);
+        pizzaOrder = new PizzaOnionsDecorator(pizzaOrder);
+        pizzaOrder = new PizzaOnionsDecorator(pizzaOrder);
+
+
+        //When
+        BigDecimal calculatedCost = pizzaOrder.getCost();
+        String pizzaIngredients = pizzaOrder.getIngredients();
+
+        //Then
+        assertEquals(new BigDecimal(24), calculatedCost);
+        assertEquals("Pizza dough with cheese and tomato sauce + bacon + additional cheese + onions + onions", pizzaIngredients);
+    }
+
+    @Test
+    void testBuildingPizza3(){
         //Given
         PizzaOrder pizzaOrder = new BasePizza();
         pizzaOrder = new PizzaBaconDecorator(pizzaOrder);
